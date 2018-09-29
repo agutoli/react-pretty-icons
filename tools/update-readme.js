14c05007b1213ceb354163d16b449f439e7f48bd
Bruno Agutoli <bruno.agutoli@gmail.com>
2018-09-30 08:25:14 +1000

    fix

--- tools/update-readme.js
+++ tools/update-readme.js
@@ -6,10 +6,12 @@ const normalizedIconDir = `${__dirname}/../src/icons/normalized`;
 
 const updateReadme = (files) => {
   var icons = [];
+  var iconsNames = [];
   (files || []).forEach(file => {
     if (/.js$/.test(file)) return;
     const filename = `http://s3.amazonaws.com/${process.env.S3_BUCKET}/__react-pretty-icons__/${file}.png`;
     icons.push(`![Alt text](${filename} "${file}")`);
+    iconsNames.push(file)
   });
 
   var source = `|   -  |   A   |   B   |   C   |   D   |   E   |   F   |   G   |   H   |   I   |   J   |   K   |   L   |\n`;
@@ -27,6 +29,21 @@ const updateReadme = (files) => {
         source += `${icon} | \n`;
         count=0;
         line++;
+
+        var count2 = 0;
+        iconsNames.forEach((name) => {
+          var iconName = name.replace('.svg', '')
+          if (count2 == 0) {
+            source += `|  -  | ${iconName} | `;
+            count2++;
+          } else if (count < 11) {
+            source += `${iconName} | `;
+            count2++;
+          } else if (count === 11) {
+            source += `${iconName} | \n`;
+            count2=0;
+          }
+        })
       }
   })
 
